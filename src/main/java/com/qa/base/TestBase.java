f1c84af367861e3a5be0295a97a3c577ff5274eb
patanshafiqa <patanshafiqa@gmail.com>
2023-12-01 11:14:57 +0530

    Added new registration page

--- src/main/java/com/qa/base/TestBase.java
+++ src/main/java/com/qa/base/TestBase.java
@@ -38,7 +38,7 @@ public class TestBase {
 	{
 		String browser=pr.getProperty("browser");
 		if (browser.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", "C:\\Users\\patan\\Downloads\\chromedriver_win32\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", "C:\\Users\\patan\\Downloads\\chromedriver_win32\\chromedriver.exe");
 			driver=new ChromeDriver();
 			
 		}
